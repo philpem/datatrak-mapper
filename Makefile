77458546b767bda8cbf6f28970600dbcc15381e9
Philip Pemberton <philpem@philpem.me.uk>
2017-08-28 02:38:11 +0100

    split lists of stations and active patterns

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ TGT=mapv
 OBJ=main.o
 
 CXX=g++
-CXXFLAGS+=-Wall -Wextra -I./CImg-2.0.3_pre080217
+CXXFLAGS+=-Wall -Wextra -Wno-unused-parameter -I./CImg-2.0.3_pre080217 -g -ggdb
 LDFLAGS+=-L/usr/X11R6/lib -lm -lpthread -lX11
 
 .PHONY:		all clean
--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <map>
 #include <sstream>
 #include <string>
+#include <vector>
 
 #include "CImg.h"
 
@@ -103,24 +104,78 @@ class CWorldfile {
 };
 
 
+class Transmitter;
+
+/**
+ * A single transmitted nav Slot.
+ *
+ * Tracks master-slave relationships between transmitters (base stations).
+ * The master originates the phase signal, the slave mirrors it.
+ * Stations may transmit multiple navslots (e.g. F1 and F2), so the master/slave slots must be identified.
+ */
+class Slot {
+	public:
+		Transmitter *master;			///< The master transmitter.
+		unsigned int masterSlot;		///< The slot number on the master which is being mirrored.
+		unsigned int slaveSlot;			///< The slot number which the master phase is being mirrored to.
+
+		/**
+		 * Slot constructor.
+		 *
+		 * The <b>master</b> transmitter originates the phase signal in nav slot <b>masterSlot</b>.
+		 * 
+		 * The slave transmitter (the one which carries this <b>Slot</b> in its <b>slots</b> vector)
+		 * mirrors the phase in a successive navslot.
+		 *
+		 * The <b>slots</b> vector is essentially equivalent to the Active Patterns list on a
+		 * Datatrak receiver.
+		 */
+		Slot(Transmitter *master, const unsigned int masterSlot, const unsigned int slaveSlot)
+		{
+			this->master		= master;
+			this->masterSlot	= masterSlot;
+			this->slaveSlot		= slaveSlot;
+		}
+};
+
+
+/**
+ * A Transmitter (more appropriately a ground station).
+ */
 class Transmitter
 {
 	public:
 		double			lat;
 		double			lon;
-		unsigned int	master;
 		string			name;
 		int				textAlign;	// -1 is left, 0 is centre, 1 is right
+		vector<Slot*>	slots;
 
-		Transmitter(const char *name, const unsigned int master, const double lat, const double lon, const int textAlign = -1)
+		Transmitter(const char *name, const double lat, const double lon, const int textAlign = -1)
 		{
+			this->name		= name;
 			this->lat		= lat;
 			this->lon		= lon;
-			this->master	= master;
-			this->name		= name;
 			this->textAlign	= textAlign;
 		}
+
+		void addSlot(Transmitter* master, const unsigned int masterSlot, const unsigned int slaveSlot)
+		{
+			slots.push_back(new Slot(master, masterSlot, slaveSlot));
+		}
+
+		Slot *findSlot(const unsigned int slaveSlot)
+		{
+			for (vector<Slot*>::iterator it = this->slots.begin(); it != this->slots.end(); ++it)
+			{
+				if ((*it)->slaveSlot == slaveSlot) {
+					return *it;
+				}
+			}
+			return NULL;
+		}
 };
+
 
 
 int main(int argc, char **argv)
@@ -141,41 +196,54 @@ int main(int argc, char **argv)
 
 	// define transmitters
 	map<int, Transmitter*> txes;
-	txes[1]		= new Transmitter("Huntingdon",					  0,  52.248652,  -0.34541632);
-	txes[2]		= new Transmitter("Selsey",						  1,  50.762753,  -0.79222675);
-	txes[3]		= new Transmitter("Kent",						  1,  51.005200,   0.88739097);
-	
-//	txes[4]		= new Transmitter("Roves Farm",					  1,  51.594303,  -1.6948185);
-	txes[4]		= new Transmitter("Stratford-upon-Avon",		  1,  52.147024,  -1.5071244, TEXTALIGN_RIGHT);		// alternate 4
-	
-	txes[5]		= new Transmitter("Lowestoft",					  1,  52.581448,   1.5563448, TEXTALIGN_RIGHT);
-	
-	txes[6]		= new Transmitter("Asserby",					  4,  53.273217,   0.23136825);	// slot A
-	txes[16]	= new Transmitter("Asserby",					 10,  53.273217,   0.23136825, TEXTALIGN_RIGHT);	// slot B
-	
-	txes[7]		= new Transmitter("Southport",					  1,  53.620775,  -2.9806128);	// Slot A
-	txes[13]	= new Transmitter("Southport",					 10,  53.620775,  -2.9806128, TEXTALIGN_RIGHT);	// Slot B
-
-	txes[8]		= new Transmitter("Cowbridge",					  4,  51.442644,  -3.4975458);
-
+	txes[1]		= new Transmitter("Huntingdon",					52.248652,  -0.34541632);
+	txes[2]		= new Transmitter("Selsey",						50.762753,  -0.79222675);
+	txes[3]		= new Transmitter("Kent",						51.005200,   0.88739097);
+//	txes[4]		= new Transmitter("Roves Farm",					51.594303,  -1.6948185);
+	txes[4]		= new Transmitter("Stratford-upon-Avon",		52.147024,  -1.5071244, TEXTALIGN_RIGHT);		// alternate 4
+	txes[5]		= new Transmitter("Lowestoft",					52.581448,   1.5563448, TEXTALIGN_RIGHT);
+	txes[6]		= new Transmitter("Asserby",					53.273217,   0.23136825);	// slot A
+	txes[7]		= new Transmitter("Southport",					53.620775,  -2.9806128);	// Slot A
+	txes[8]		= new Transmitter("Cowbridge",					51.442644,  -3.4975458);
 //	txes[9]		= new Transmitter("Swindon",					 10,  51.650225,  -1.6733554);
-
-	txes[10]	= new Transmitter("Cumbria",					  7,  54.862266,  -3.1868761);
-
-	txes[11]	= new Transmitter("Greenock",					 10,  55.935688,  -4.8527571);
-
-	txes[12]	= new Transmitter("Alnwick",					 10,  55.354577,  -1.7026607);
+	txes[10]	= new Transmitter("Cumbria",					54.862266,  -3.1868761);
+	txes[11]	= new Transmitter("Greenock",					55.935688,  -4.8527571);
+	txes[12]	= new Transmitter("Alnwick",					55.354577,  -1.7026607);
 
 	// Filey/Muston, 13 and 19. 13 is Southport-B
 	// Don't know the master station so it isn't listed
 	// Need master information (Active Patterns) from #3675RX.
 
-	txes[14]	= new Transmitter("Kingsbridge",				  4,  50.221003,  -3.7279835);
+	txes[14]	= new Transmitter("Kingsbridge",				50.221003,  -3.7279835);
+	txes[15]	= new Transmitter("Stonehaven",					56.839071,  -2.3556843);
 
-	txes[15]	= new Transmitter("Stonehaven",					 10,  56.839071,  -2.3556843);
+
+	txes[1]->addSlot(NULL, 0, 1);
+	txes[2]->addSlot(txes[1], 1, 2);
+	txes[3]->addSlot(txes[1], 1, 3);
+	txes[4]->addSlot(txes[1], 1, 4);
+	txes[5]->addSlot(txes[1], 1, 5);
+
+	// Southport and Asserby are both multihomed (F1/F2 transition) transmitters.
+	// This means they transmit an F1 and an F2 slot, and need to be added last.
+	txes[6]->addSlot(txes[4], 4, 6);			// slot A
+	txes[6]->addSlot(txes[10], 10, 16);		// slot B
+	
+	txes[7]->addSlot(txes[1], 1, 7);			// slot A
+	txes[7]->addSlot(txes[10], 10, 13);		// slot B
+
+	txes[8]->addSlot(txes[4], 4, 8);
+//	txes[9]->addSlot(txes[1], 10, 9);
+	txes[10]->addSlot(txes[7], 7, 10);
+	txes[11]->addSlot(txes[10], 10, 11);
+	txes[12]->addSlot(txes[10], 10, 12);
+	txes[14]->addSlot(txes[4], 4, 14);
+	txes[15]->addSlot(txes[10], 10, 15);
+
 
 	// marker colour
 	unsigned char markerColours[][3] = {
+		{ 255, 255, 255 },
 		{ 255,   0,   0 },
 		{ 255, 255,   0 },
 		{   0, 255,   0 },
@@ -189,55 +257,74 @@ int main(int argc, char **argv)
 		long master_x, master_y;
 		long me_x, me_y;
 
+		// used to build the tx label
+		stringstream txlabel;
+		txlabel << "(";
+
 		// convert lat/lon to X/Y
 		wf.coord2pix(it->second->lat, it->second->lon, me_x, me_y);
 		//me_x -= CROP_X0;
 		//me_y -= CROP_Y0;
 
 		cout << "plotting " << it->second->name << "; lon " << it->second->lon << ", lat " << it->second->lat << endl;
-		cout << "\tX: " << me_x << endl;
-		cout << "\tY: " << me_y << endl;
+		cout << "\tX, Y: " << me_x << ", " << me_y << endl;
 
-		// figure out how many times the signal has been relayed
-		int relayDepth = 0;
-		int nextHop = it->second->master;
-		while (nextHop != 0) {
-			nextHop = txes[nextHop]->master;
-			relayDepth++;
+
+		// Scan all the slots this transmitter provides
+		const char *sep = "";
+		for (vector<Slot*>::iterator si=it->second->slots.begin(); si!=it->second->slots.end(); ++si)
+		{
+			// figure out how many times the signal has been relayed
+			int relayDepth = 0;
+			Slot *s = *si;
+			cout << "\tslot " << s->slaveSlot << " path:" << endl;
+
+			txlabel << sep << s->slaveSlot;
+			sep = ",";
+
+			if (!s) {
+				cout << "ERROR - cannot resolve slot list for this TX" << endl;
+				break;
+			}
+
+			while (s && s->masterSlot != 0) {
+				cout << "\t\t " << s->master->name << " " << s->slaveSlot << "->" << s->masterSlot << endl;
+				s = s->master->findSlot(s->masterSlot);
+				relayDepth++;
+			}
+
+			cout << "\trelay depth: " << relayDepth << endl;
+
+			// draw line to master station
+			if ((*si)->masterSlot != 0) {
+				Transmitter* ma = (*si)->master;
+				wf.coord2pix(ma->lat, ma->lon, master_x, master_y);
+
+				img.draw_line(me_x, me_y, master_x, master_y, markerColours[relayDepth]);
+			}
 		}
-		cout << "\trelay depth: " << relayDepth << endl;
-		
+		txlabel << ") " << it->second->name;
+
 		// mark the station
-		img.draw_circle(me_x, me_y, 5, markerColours[relayDepth], 1);
-
-		// draw line to master station
-		if (it->second->master != 0) {
-			Transmitter* ma = txes[it->second->master];
-			wf.coord2pix(ma->lat, ma->lon, master_x, master_y);
-
-			img.draw_line(me_x, me_y, master_x, master_y, markerColours[relayDepth-1]);
-		}
+		img.draw_circle(me_x, me_y, 5, markerColours[0], 1);
 
 		// put text on the transmitter
 		unsigned char white[] = {255,255,255};
 		unsigned char black[] = {0,0,0};
-		stringstream ss;
-		// Station alternates are identified by adding a multiple of 100 to the slot number
-		ss << "(" << it->first % 100 << ") " << it->second->name;
 
 		// draw text on a temporary image first, so we can measure the width and height
 		CImg<unsigned char> imgtext;
-		imgtext.draw_text(0,0,ss.str().c_str(),white,black,1);
+		imgtext.draw_text(0,0,txlabel.str().c_str(),white,black,1);
 
 		if (it->second->textAlign < 0) {
 			// left align
-			img.draw_text(me_x, me_y, ss.str().c_str(), white, black, TEXT_ALPHA);
+			img.draw_text(me_x, me_y, txlabel.str().c_str(), white, black, TEXT_ALPHA);
 		} else if (it->second->textAlign == 0) {
 			// centre
-			img.draw_text(me_x - (imgtext.width() / 2), me_y, ss.str().c_str(), white, black, TEXT_ALPHA);
+			img.draw_text(me_x - (imgtext.width() / 2), me_y, txlabel.str().c_str(), white, black, TEXT_ALPHA);
 		} else {
 			// right
-			img.draw_text(me_x - imgtext.width(), me_y, ss.str().c_str(), white, black, TEXT_ALPHA);
+			img.draw_text(me_x - imgtext.width(), me_y, txlabel.str().c_str(), white, black, TEXT_ALPHA);
 		}
 	}
 
