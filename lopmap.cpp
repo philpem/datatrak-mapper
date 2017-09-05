003619fd81ab5946498f2e2379c444c9dec05b08
Philip Pemberton <philpem@philpem.me.uk>
2017-09-06 00:08:46 +0100

    lopmap: plot the LOPs on the map, not a black image. code cleanup.
    Plot the LOPs on the map, along with the transmitter dots.
    
    Code cleanup - move some configurable constants to the top of
    the C++ source file.

--- lopmap.cpp
+++ lopmap.cpp
@@ -26,9 +26,27 @@ const long CROP_Y0 = 1370;				// Crop, Y pixel coord, top-left corner
 const long CROP_X1 = CROP_X0 + 585;		// Crop, X pixel coord, bottom-right corner
 const long CROP_Y1 = CROP_Y0 + 445;		// Crop, Y pixel coord, bottom-right corner
 
+const float TEXT_ALPHA = 0.5f;				///< Text transparency level
+
+const double BLANKING_DISTANCE = 200.0e3;	///< Blanking distance in metres. If distance to master or slave exceeds this, the LOP will not be plotted.
+const double LOP_RANGE = 15.0;				///< Plot the LOP only if its phase is within this number of degrees of zero.
+const double FREQ = 122.0e3 / 10.;			///< Transmitter frequency. Divided by ten so we only show every 10th LOP.
+
+const double SPEED_OF_LIGHT = 299792458;	///< Speed of light in metres per second.
+
+const unsigned char MARKERCOLOURS[][3] = {
+	{ 255, 255, 255 },
+	{ 255,   0,   0 },
+	{ 255, 255,   0 },
+	{   0, 255,   0 },
+	{   0, 255, 255 },
+	{   0,   0, 255 }
+};
+
+const unsigned char white[] = {255,255,255};
+const unsigned char black[] = {0,0,0};
 
 
-const float TEXT_ALPHA = 0.5f;
 
 
 
@@ -40,21 +58,15 @@ int main(int argc, char **argv)
 
 	// load map file
 	CImg<unsigned char> img(MAP_FILE);
+	// crop the map
+	img.crop(CROP_X0, CROP_Y0, CROP_X1, CROP_Y1);
+
 
 	// define transmitters
 	map<int, Transmitter*> txes;
 	initTransmitters(txes);
 
 
-	// marker colour
-	unsigned char markerColours[][3] = {
-		{ 255, 255, 255 },
-		{ 255,   0,   0 },
-		{ 255, 255,   0 },
-		{   0, 255,   0 },
-		{   0, 255, 255 },
-		{   0,   0, 255 }
-	};
 
 	// plot txes on map
 	for (map<int, Transmitter*>::iterator it=txes.begin(); it!=txes.end(); ++it)
@@ -67,8 +79,8 @@ int main(int argc, char **argv)
 
 		// convert lat/lon to X/Y
 		wf.coord2pix(it->second->lat, it->second->lon, me_x, me_y);
-		//me_x -= CROP_X0;
-		//me_y -= CROP_Y0;
+		me_x -= CROP_X0;
+		me_y -= CROP_Y0;
 
 		cout << "plotting " << it->second->name << "; lon " << it->second->lon << ", lat " << it->second->lat << endl;
 		cout << "\tX, Y: " << me_x << ", " << me_y << endl;
@@ -106,7 +118,7 @@ int main(int argc, char **argv)
 				Transmitter* ma = (*si)->master;
 				wf.coord2pix(ma->lat, ma->lon, master_x, master_y);
 
-				img.draw_line(me_x, me_y, master_x, master_y, markerColours[relayDepth]);
+				img.draw_line(me_x, me_y, master_x, master_y, MARKERCOLOURS[relayDepth]);
 			}
 		}
 		txlabel << ") " << it->second->name;
@@ -114,11 +126,9 @@ int main(int argc, char **argv)
 		*/
 
 		// mark the station
-		img.draw_circle(me_x, me_y, 5, markerColours[0], 1);
+		img.draw_circle(me_x, me_y, 5, MARKERCOLOURS[0], 1);
 
 		// put text on the transmitter
-		unsigned char white[] = {255,255,255};
-		unsigned char black[] = {0,0,0};
 
 		// draw text on a temporary image first, so we can measure the width and height
 		CImg<unsigned char> imgtext;
@@ -134,8 +144,10 @@ int main(int argc, char **argv)
 			// right
 			img.draw_text(me_x - imgtext.width(), me_y, it->second->name.c_str(), white, black, TEXT_ALPHA);
 		}
-
 	}
+
+
+	// plot all the baselines and LOPs
 
 	// select the baseline to plot
 //	txa = txes[1];
@@ -148,11 +160,8 @@ int main(int argc, char **argv)
 	double b_lon = txes[2]->lon;
 
 
-	// create a temporary CImg to store our map
-	CImg<float> basemap(CROP_X1-CROP_X0, CROP_Y1-CROP_Y0);
-
 	// iterate over X/Y pixels
-	cimg_forXY(basemap, x, y) {
+	cimg_forXY(img, x, y) {
 		// convert X/Y offset to lon/lat
 		double lon, lat;
 		wf.pix2coord(x+CROP_X0, y+CROP_Y0, lon, lat);
@@ -162,47 +171,36 @@ int main(int argc, char **argv)
 		double dist_b = vincenty_distance(b_lat, b_lon, lat, lon) * 1000.0;
 
 #if 1
-		// abort if distance exceeds 200km (Datatrak Locator blanking distance)
+		// skip this point if the distance to TX A or TX B exceeds 200km
+		// (Datatrak Locator blanking distance)
 		if ((dist_a > 200.0e3) || (dist_b > 200.0e3)) {
-			basemap(x, y) = 0.0;
+			//basemap(x, y) = 0.0;
 			continue;
 		}
 #endif
 
-	//	cout << "lon " << lon << " lat " << lat;
-	//	cout << "   distA " << dist_a << endl;
-		//cout << "   distB " << dist_b << endl;
-
-		// calculate propagation delay
-		const double SPEED_OF_LIGHT = 299792458;	// metres per second
 
 		// calculate difference in distance
 		double dist_diff = abs(dist_a - dist_b);
 
-		// calculate phase shift
-		const double FREQ = 122.0e3 / 10.;	// 122 kHz -- show only every 10th LOP
+		// calculate phase shift (via the differential distance)
 		const double CYCTIME = 1.0 / FREQ;
 
 		double propagation_time = dist_diff / SPEED_OF_LIGHT;	// seconds
-		double phase_shift = fmod(propagation_time, CYCTIME) * (360. / CYCTIME);
+		double phase_shift = fmod(propagation_time, CYCTIME) * (360.0 / CYCTIME);
 
-		// plot!
-		const double RANGE = 45.0;
-		if ((phase_shift < RANGE) || (phase_shift > (360.0 - RANGE))) {
-			basemap(x,y) = phase_shift;
-		} else {
-			basemap(x,y) = 0.0;
+		// plot only if LOP phase is within +/- <RANGE> of zero
+		if ((phase_shift < LOP_RANGE) || (phase_shift > (360.0 - LOP_RANGE))) {
+			img(x,y,0) = MARKERCOLOURS[0][0];// * (phase_shift * 255.0 / 360.0);
+			img(x,y,1) = MARKERCOLOURS[0][1];// * (phase_shift * 255.0 / 360.0);
+			img(x,y,2) = MARKERCOLOURS[0][2];// * (phase_shift * 255.0 / 360.0);
 		}
-
-		//basemap(x, y) = phase_shift;
 	}
 
 
 //	basemap.normalize(0,255);
-	basemap.display("LOPs");
+//	basemap.display("LOPs");
 
-	// crop the map image appropriately (default is to cover the UK only)
-//	img.crop(CROP_X0, CROP_Y0, CROP_X1, CROP_Y1);
-//	img.display("Map");
+	img.display("Datatrak transmitters with LOPs shown");
 
 }
