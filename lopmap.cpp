6597e65baf1765938b90fa98b172f42ffe6107af
Philip Pemberton <philpem@philpem.me.uk>
2017-09-06 07:53:18 +0100

    LOPMap: use better textures, many improvements
      * Use the NACIS Natural Earth v1.3 1:10m texture pack from
        http://www.naturalearthdata.com/ (instead of NE3).
        This produces higher resolution maps.
    
      * Explain how to configure LOPMap blanking distance.
    
      * Fix the names of some constants which had lower-case names.
    
      * Move LOP plotting into a separate function.
    
      * Plot LOP lines with transparency.

--- lopmap.cpp
+++ lopmap.cpp
@@ -18,6 +18,7 @@ using namespace std;
 using namespace cimg_library;
 
 
+/*
 const char * MAP_FILE = "natural_earth_3/2_no_clouds_16k.jpg";		// JPEG/PNG/etc file containing the map
 const char * TFW_FILE = "natural_earth_3/2_no_clouds_16k.tfw";		// Esri worldfile for the map
 
@@ -25,10 +26,29 @@ const long CROP_X0 = 7615;				// Crop, X pixel coord, top-left corner
 const long CROP_Y0 = 1370;				// Crop, Y pixel coord, top-left corner
 const long CROP_X1 = CROP_X0 + 585;		// Crop, X pixel coord, bottom-right corner
 const long CROP_Y1 = CROP_Y0 + 445;		// Crop, Y pixel coord, bottom-right corner
+*/
+const char *MAP_FILE = "HYP_HR_SR_W/HYP_HR_SR_W.tif";
+const char *TFW_FILE = "HYP_HR_SR_W/HYP_HR_SR_W.tfw";
+const long CROP_X0 = 10153;				// Crop, X pixel coord, top-left corner
+const long CROP_Y0 = 1827;				// Crop, Y pixel coord, top-left corner
+const long CROP_X1 = CROP_X0 + 780;		// Crop, X pixel coord, bottom-right corner
+const long CROP_Y1 = CROP_Y0 + 593;		// Crop, Y pixel coord, bottom-right corner
 
-const float TEXT_ALPHA = 0.5f;				///< Text transparency level
 
-const double BLANKING_DISTANCE = 200.0e3;	///< Blanking distance in metres. If distance to master or slave exceeds this, the LOP will not be plotted.
+const float TEXT_ALPHA = 0.5f;				///< Text transparency
+const float LOP_ALPHA = 0.75f;				///< LOP transparency
+
+/**
+ * Blanking distance in metres.
+ *
+ * If distance to master or slave station exceeds this, the LOP will not be
+ * plotted.
+ *
+ * Set this to positive infinity to disable blanking (note your LOP graph will
+ * be very messy!).
+ */
+const double BLANKING_DISTANCE = 200.0e3;	
+
 const double LOP_RANGE = 15.0;				///< Plot the LOP only if its phase is within this number of degrees of zero.
 const double FREQ = 122.0e3 / 10.;			///< Transmitter frequency. Divided by ten so we only show every 10th LOP.
 
@@ -43,16 +63,70 @@ const unsigned char MARKERCOLOURS[][3] = {
 	{   0,   0, 255 }
 };
 
-const unsigned char white[] = {255,255,255};
-const unsigned char black[] = {0,0,0};
+const unsigned char WHITE[] = {255,255,255};
+const unsigned char BLACK[] = {0,0,0};
 
 
+
+void plotLOP(CImg<unsigned char> &img, const CWorldfile &wf, const Transmitter *txA, const Transmitter *txB, const unsigned int colour = 0)
+{
+	// get A/B transmitter lon/lat
+	double a_lat = txA->lat;
+	double a_lon = txA->lon;
+	double b_lat = txB->lat;
+	double b_lon = txB->lon;
+
+
+	// iterate over X/Y pixels
+	cimg_forXY(img, x, y) {
+		// convert X/Y offset to lon/lat
+		double lon, lat;
+		wf.pix2coord(x+CROP_X0, y+CROP_Y0, lon, lat);
+
+		// calculate great-circle distance in metres
+		double dist_a = vincenty_distance(a_lat, a_lon, lat, lon) * 1000.0;
+		double dist_b = vincenty_distance(b_lat, b_lon, lat, lon) * 1000.0;
+
+		// skip this point if the distance to TX A or TX B exceeds 200km
+		// (Datatrak Locator blanking distance)
+		if ((dist_a > BLANKING_DISTANCE) || (dist_b > BLANKING_DISTANCE)) {
+			//basemap(x, y) = 0.0;
+			continue;
+		}
+
+
+		// calculate difference in distance
+		double dist_diff = abs(dist_a - dist_b);
+
+		// calculate propagation time (for distance-difference)
+		double propagation_time = dist_diff / SPEED_OF_LIGHT;	// seconds
+
+		// calculate phase shift (divide instead of fmod will get the lane number)
+		double phase_shift = fmod(propagation_time, (1.0 / FREQ)) * (360.0 / (1.0 / FREQ));
+
+		// plot only if LOP phase is within +/- <RANGE> of zero
+		if ((phase_shift < LOP_RANGE) || (phase_shift > (360.0 - LOP_RANGE))) {
+			img.draw_point(x, y, MARKERCOLOURS[colour], LOP_ALPHA);
+			//img(x,y,2) = MARKERCOLOURS[colour][2];// * (phase_shift * 255.0 / 360.0);
+		}
+	}
+
+
+	// Plot the baseline too
+	long ax, ay, bx, by;
+	wf.coord2pix(a_lat, a_lon, ax, ay);
+	wf.coord2pix(b_lat, b_lon, bx, by);
+	ax -= CROP_X0;
+	bx -= CROP_X0;
+	ay -= CROP_Y0;
+	by -= CROP_Y0;
+	img.draw_line(ax, ay, bx, by, MARKERCOLOURS[colour]);
+}
 
 
 
 int main(int argc, char **argv)
 {
-
 	// read the worldfile
 	CWorldfile wf(TFW_FILE);
 
@@ -81,10 +155,6 @@ int main(int argc, char **argv)
 		wf.coord2pix(it->second->lat, it->second->lon, me_x, me_y);
 		me_x -= CROP_X0;
 		me_y -= CROP_Y0;
-
-		cout << "plotting " << it->second->name << "; lon " << it->second->lon << ", lat " << it->second->lat << endl;
-		cout << "\tX, Y: " << me_x << ", " << me_y << endl;
-
 
 		/*
 
@@ -132,17 +202,17 @@ int main(int argc, char **argv)
 
 		// draw text on a temporary image first, so we can measure the width and height
 		CImg<unsigned char> imgtext;
-		imgtext.draw_text(0,0,it->second->name.c_str(),white,black,1);
+		imgtext.draw_text(0, 0, it->second->name.c_str(), WHITE, BLACK, 1);
 
 		if (it->second->textAlign < 0) {
 			// left align
-			img.draw_text(me_x, me_y, it->second->name.c_str(), white, black, TEXT_ALPHA);
+			img.draw_text(me_x, me_y, it->second->name.c_str(), WHITE, BLACK, TEXT_ALPHA);
 		} else if (it->second->textAlign == 0) {
 			// centre
-			img.draw_text(me_x - (imgtext.width() / 2), me_y, it->second->name.c_str(), white, black, TEXT_ALPHA);
+			img.draw_text(me_x - (imgtext.width() / 2), me_y, it->second->name.c_str(), WHITE, BLACK, TEXT_ALPHA);
 		} else {
 			// right
-			img.draw_text(me_x - imgtext.width(), me_y, it->second->name.c_str(), white, black, TEXT_ALPHA);
+			img.draw_text(me_x - imgtext.width(), me_y, it->second->name.c_str(), WHITE, BLACK, TEXT_ALPHA);
 		}
 	}
 
@@ -153,49 +223,11 @@ int main(int argc, char **argv)
 //	txa = txes[1];
 //	txb = txes[2];
 
-	// get A/B transmitter lon/lat
-	double a_lat = txes[1]->lat;
-	double a_lon = txes[1]->lon;
-	double b_lat = txes[2]->lat;
-	double b_lon = txes[2]->lon;
-
-
-	// iterate over X/Y pixels
-	cimg_forXY(img, x, y) {
-		// convert X/Y offset to lon/lat
-		double lon, lat;
-		wf.pix2coord(x+CROP_X0, y+CROP_Y0, lon, lat);
-
-		// calculate great-circle distance in metres
-		double dist_a = vincenty_distance(a_lat, a_lon, lat, lon) * 1000.0;
-		double dist_b = vincenty_distance(b_lat, b_lon, lat, lon) * 1000.0;
-
-#if 1
-		// skip this point if the distance to TX A or TX B exceeds 200km
-		// (Datatrak Locator blanking distance)
-		if ((dist_a > 200.0e3) || (dist_b > 200.0e3)) {
-			//basemap(x, y) = 0.0;
-			continue;
-		}
-#endif
-
-
-		// calculate difference in distance
-		double dist_diff = abs(dist_a - dist_b);
-
-		// calculate phase shift (via the differential distance)
-		const double CYCTIME = 1.0 / FREQ;
-
-		double propagation_time = dist_diff / SPEED_OF_LIGHT;	// seconds
-		double phase_shift = fmod(propagation_time, CYCTIME) * (360.0 / CYCTIME);
-
-		// plot only if LOP phase is within +/- <RANGE> of zero
-		if ((phase_shift < LOP_RANGE) || (phase_shift > (360.0 - LOP_RANGE))) {
-			img(x,y,0) = MARKERCOLOURS[0][0];// * (phase_shift * 255.0 / 360.0);
-			img(x,y,1) = MARKERCOLOURS[0][1];// * (phase_shift * 255.0 / 360.0);
-			img(x,y,2) = MARKERCOLOURS[0][2];// * (phase_shift * 255.0 / 360.0);
-		}
-	}
+	plotLOP(img, wf, txes[1], txes[2], 0);
+	plotLOP(img, wf, txes[1], txes[3], 1);
+	plotLOP(img, wf, txes[1], txes[4], 2);
+	plotLOP(img, wf, txes[1], txes[5], 3);
+	plotLOP(img, wf, txes[1], txes[7], 4);
 
 
 //	basemap.normalize(0,255);
